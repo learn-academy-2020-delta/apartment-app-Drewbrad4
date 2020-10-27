6230ff47ef97d8251d015a2333ca36c5b556443d
drewbrad4 <drewbrad4@gmail.com>
2020-10-27 16:49:26 -0700

    added lots of styling

--- app/javascript/components/Pages/ApartmentShow.js
+++ app/javascript/components/Pages/ApartmentShow.js
@@ -2,10 +2,21 @@ import React, { Component } from 'react';
 
 export default class ApartmentShow extends Component{
     render(){
+        const {apartment} = this.props
         return(
-            <>
-                <h3>Show</h3>
-            </>
+            <div className="show">    
+                <div className="apartmentCard">
+                    <h3 className="heading">{apartment.street}</h3>
+                    <p className="apt-info">City: {apartment.city}</p>
+                    <p className="apt-info">State: {apartment.state}</p>
+                    <p className="apt-info">Manager: {apartment.manager}</p>
+                    <p className="apt-info">Email: {apartment.email}</p>
+                    <p className="apt-info">Price: {apartment.price}</p>
+                    <p className="apt-info">Bedrooms: {apartment.bedrooms}</p>
+                    <p className="apt-info">Bathrooms: {apartment.bathrooms}</p>
+                    <p className="apt-info">Pets: {apartment.pets}</p>
+                </div>
+            </div>
         )
     }
 }
